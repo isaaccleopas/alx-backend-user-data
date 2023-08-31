bb02efbe8d96f28ed60619157d8d52816ec1c307
isaaccleopas <isaaccleopas@gmail.com>
2023-08-31 06:06:04 +0000

    task 1 added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,24 @@ def filter_datum(
     """Filters log line"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Instantiate the arguements"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format a LogRecord"""
+        log_msg = super(RedactingFormatter, self).format(record)
+        return (filter_datum(self.fields, self.REDACTION, log_msg,
+                self.SEPARATOR))
