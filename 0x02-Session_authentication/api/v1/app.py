9143aea59bf5b988c38d27d6543e68f8474c6ac2
isaaccleopas <isaaccleopas@gmail.com>
2023-09-06 19:10:37 +0000

    app.py corrected

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,12 +2,12 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
